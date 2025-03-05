0d7b6c985361c671d86af4b935b24313e4af5eae
Rumit-Mehta <rumit.m.uk@gmail.com>
2025-03-05 17:07:31 +0800

    Feat: get latest transaction date and save it to a text file

--- finance/monzo/monzo_api.py
+++ finance/monzo/monzo_api.py
@@ -5,7 +5,6 @@ from finance.constants import ACCESS_TOKEN
 
 logger = logging.getLogger(__name__)
 
-
 # Fetch account ID
 def get_account_id():
     url = "https://api.monzo.com/accounts"
@@ -21,7 +20,6 @@ def get_account_id():
             return accounts[0]["id"]  # Using the first account
     raise Exception("Failed to retrieve Monzo account ID.")
 
-
 # Fetch transactions
 def get_transactions(account_id, date_from):
     url = f"https://api.monzo.com/transactions"
@@ -35,8 +33,23 @@ def get_transactions(account_id, date_from):
     response = requests.get(url, headers=headers, params=params)
     
     if response.status_code == 200:
+        transactions = response.json()["transactions"]    
+        
+        # if transactions isnt empty
+        if transactions:
+            # Get the date of the last transaction (assuming the list is in chronological order)
+            last_transaction_date = transactions[-1]["created"]
+
+            # Append the date to a text file
+            with open("files/monzo_last_transaction_date.txt", "w") as file:
+                file.write(f"{last_transaction_date}\n")
+                logging.info(f"Last transaction date saved: {last_transaction_date}")
+        else: 
+            logging.info("No transactions found.")
+
         logging.info("DONE 2/4  - Got transactions from Monzo API")
         return response.json()["transactions"]
+    
     raise Exception("Failed to retrieve transactions.")
 
 # Convert transactions to a DataFrame
