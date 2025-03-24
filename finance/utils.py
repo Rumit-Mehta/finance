dfc485ceae0ec5d692a95db5a53e9f67b25e74b8
Rumit-Mehta <rumit.m.uk@gmail.com>
2025-03-24 14:14:32 +0800

    chore: unnecesarry def removed

--- finance/utils.py
+++ finance/utils.py
@@ -1,55 +1,17 @@
 import logging
 import os
 from finance import constants
-import files
 import xlwings as xw
-from openpyxl import load_workbook
-from openpyxl.styles import Font, Alignment
-from datetime import timedelta
+
 
 
 EXCEL_FILE = constants.EXCEL_FILE
 SHEET_NAME = constants.SHEET_NAME
 
 
-# get the date for the last entry for a spefific account eg. monzo, amex etc
-def latest_entry(account: str):
-
-    # load the workbook and sheet
-    workbook = load_workbook(EXCEL_FILE)
-    ws = workbook["Tracking"]
-
-    # get the indecies for the date and account column
-    date_column = 2
-    account_column = 8
-
-    # reverse iterate through the sheet
-    latest_date = None
-    for row in reversed(
-        list(ws.iter_rows(min_row=12, values_only=True))
-    ):  # Start from row 12
-        date_value, account_value = (
-            row[date_column],
-            row[account_column],
-        )  # Adjust based on actual columns
-        if account_value and account in str(account_value):
-            latest_date = date_value
-            break
-
-    # Print the latest date found
-    if latest_date:
-        print("Latest Monzo transaction date:", latest_date)
-        print("Day after Monzo transaction date:", latest_date + timedelta(days=1))
-        # you are more interested in the start date which is 1 more than the latest date.
-        latest_date = str(latest_date + timedelta(days=1)).split(" ")[0]
-    else:
-        print("No Monzo tag found in the file.")
-
-    return latest_date
-
-
 def latest_entry_file(file, account):
     """Account used only for logging purposes"""
+    print(file)
     with open(file, "r") as file:
         latest_date = file.read()
         logging.info(f"{account} - Transactions From: {latest_date}")
